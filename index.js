c5ab26884c3826a29bdb1fb1d4374007697afebd
harezora <155614414+sunnybluesky@users.noreply.github.com>
2025-05-21 08:05:23 +0000

    a

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const socketio = new Server(server);
+
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/public/index.html');
+});
+
+server.listen(3000);
+
+socketio.on('connection',(socket)=>{
+   // ルームに参加する
+   socket.on('join-room', (data)=>{
+      socket.join(data.roomId);
+   });
+   // 待ち受け
+   socket.on('send-room-msg', (data)=>{
+      // クライアントに送信
+      socketio.to(data.roomId).emit('msg', data);
+      socket.to(data.roomId).emit('msg', data);
+      socket.emit("msg",data)
+   });
+
+   
+   
+   // クライアントが切断したときの処理
+   socket.on('disconnect', () => {
+      console.log('Disconnected:', socket.id);
+   });
+})

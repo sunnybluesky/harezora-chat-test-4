ad636fec04a8aff717bbf9120c57f8c60c390b58
harezora <155614414+sunnybluesky@users.noreply.github.com>
2025-05-21 07:29:45 +0000

    Socket!!!

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+// official
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const socketio = new Server(server);

521f6230e04a37f0060947509a04409a7551289a
harezora <155614414+sunnybluesky@users.noreply.github.com>
2025-05-21 08:35:48 +0000

    a

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-// official
 const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const socketio = new Server(server);
+const socketIo = require('socket.io');
 
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/public/index.html');
 });
 
-server.listen(3000);
+server.listen(3000,()=>{
+    io.emit("reload")
+});
 
-socketio.on('connection',(socket)=>{
+io.on('connection',(socket)=>{
    // ルームに参加する
    socket.on('join-room', (data)=>{
       socket.join(data.roomId);
@@ -21,9 +22,7 @@ socketio.on('connection',(socket)=>{
    // 待ち受け
    socket.on('send-room-msg', (data)=>{
       // クライアントに送信
-      socketio.to(data.roomId).emit('msg', data);
-      socket.to(data.roomId).emit('msg', data);
-      socket.emit("msg",data)
+      io.to(data.roomId).emit('msg', data);
    });
 
    
